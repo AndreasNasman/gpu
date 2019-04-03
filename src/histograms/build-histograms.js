0c73a288a6a38bafdaebab70ac87f4efe9ea236e
Andreas Näsman <andreasnasman@Andreass-MacBook-Pro.local>
2019-04-03 15:50:52 +0300

    Adds histogram building

---
+++ src/histograms/build-histograms.js
@@ -0,0 +1,58 @@
+exports.execute = () => {
+  console.log("\x1b[33m%s\x1b[0m", "Building of histograms started.");
+
+  const fs = require("fs");
+
+  const realGalaxies = require("../input-data/real-galaxies.json");
+  const randomGalaxies = require("../input-data/random-galaxies.json");
+
+  const COVERAGE = 180; // degrees
+  const BIN_WIDTH = 0.25; // degress
+  const NUMBER_OF_BINS = COVERAGE * (1 / BIN_WIDTH);
+
+  const buildHistogram = (firstGalaxySet, secondGalaxySet, outputName) => {
+    const bins = Array(NUMBER_OF_BINS).fill(0);
+
+    const angleBetweenTwoGalaxies = (firstGalaxy, secondGalaxy) =>
+      Math.acos(
+        Math.sin(firstGalaxy.declination) * Math.sin(secondGalaxy.declination) +
+          Math.cos(firstGalaxy.declination) *
+            Math.cos(secondGalaxy.declination) *
+            Math.cos(firstGalaxy.rightAscension - secondGalaxy.rightAscension)
+      );
+
+    const radiansToDegrees = radians => radians * (180 / Math.PI);
+
+    const updateBins = angle => {
+      if (isNaN(angle)) return;
+      const index = Math.floor(radiansToDegrees(angle) / BIN_WIDTH);
+      bins[index] += 1;
+    };
+
+    const SIZE = 10000;
+    if (!secondGalaxySet) {
+      firstGalaxySet.slice(0, SIZE).forEach((firstGalaxy, index) => {
+        firstGalaxySet.slice(index, SIZE).forEach(secondGalaxy => {
+          const angle = angleBetweenTwoGalaxies(firstGalaxy, secondGalaxy);
+          updateBins(angle);
+        });
+      });
+    } else {
+      firstGalaxySet.slice(0, SIZE).forEach(firstGalaxy => {
+        secondGalaxySet.slice(0, SIZE).forEach(secondGalaxy => {
+          const angle = angleBetweenTwoGalaxies(firstGalaxy, secondGalaxy);
+          updateBins(angle);
+        });
+      });
+    }
+
+    fs.writeFileSync(`${__dirname}/${outputName}.json`, JSON.stringify(bins));
+    console.log(`Histogram '${outputName}' built!`);
+  };
+
+  buildHistogram(realGalaxies, null, "histogram-DD");
+  buildHistogram(realGalaxies, randomGalaxies, "histogram-DR");
+  buildHistogram(randomGalaxies, null, "histogram-RR");
+
+  console.log("\x1b[33m%s\x1b[0m", "Building of histograms started.\n");
+};
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-console.clear();
-
-const convertInputData = require("./input-data/convert-input-data");
-
 const hrstart = process.hrtime();
 
+const convertInputData = require("./input-data/convert-input-data");
 convertInputData.execute();
+
+const buildHistograms = require("./histograms/build-histograms");
+buildHistograms.execute();
 
 const hrend = process.hrtime(hrstart);
 console.info("Execution time (hr): %ds %dms\n", hrend[0], hrend[1] / 1000000);
