9edd45c6f182363822a909624f27e1ad6fec6830
Andreas Näsman <andreas.nasman92@gmail.com>
2019-08-03 14:14:00 +0300

    Refactorizations and commenting

--- src/c/main.cu
+++ src/c/main.cu
@@ -21,8 +21,9 @@ typedef struct GalaxySet
     Galaxy *galaxies;
 } GalaxySet;
 
-__global__ void collect_histograms(GalaxySet real, GalaxySet random, int *DD_histogram, int *DR_histogram, int *RR_histogram, int n);
+__global__ void collect_histograms(GalaxySet real, GalaxySet random, int *DD_histogram_collected, int *DR_histogram_collected, int *RR_histogram_collected, int n);
 __global__ void measure_galaxy_distribution(int *DD_histogram, int *DR_histogram, int *RR_histogram, float *distribution, int n);
+void accumulate_histograms(int *DD_histogram_collected, int *DR_histogram_collected, int *RR_histogram_collected, int *DD_histogram, int *DR_histogram, int *RR_histogram, int n);
 void read_file(FILE *file_pointer, const char *DELIMITER, Galaxy *galaxy_set, int n);
 void write_file_int(FILE *file_pointer, int *content, int n);
 void write_file_float(FILE *file_pointer, float *content, int n);
@@ -78,12 +79,7 @@ int main()
     cudaMallocManaged(&DR_histogram, BINS_TOTAL * sizeof(int));
     cudaMallocManaged(&RR_histogram, BINS_TOTAL * sizeof(int));
 
-    for (int i = 0; i < COLLECTED_HISTOGRAM_SIZE; i += 1)
-    {
-        DD_histogram[i % BINS_TOTAL] += DD_histogram_collected[i];
-        DR_histogram[i % BINS_TOTAL] += DR_histogram_collected[i];
-        RR_histogram[i % BINS_TOTAL] += RR_histogram_collected[i];
-    }
+    accumulate_histograms(DD_histogram_collected, DR_histogram_collected, RR_histogram_collected, DD_histogram, DR_histogram, RR_histogram, COLLECTED_HISTOGRAM_SIZE);
 
     /* DETERMINING DISTRIBUTION */
     float *distribution;
@@ -126,7 +122,9 @@ __device__ float angle_between_two_galaxies(Galaxy first_galaxy, Galaxy second_g
                   cosf(first_galaxy.right_ascension - second_galaxy.right_ascension);
 
     // Checks that x is within the boundaries of [-1.0f, 1.0f].
-    return acosf(fmin(1.0f, fmax(-1.0f, x)));
+    x = fmin(1.0f, fmax(-1.0f, x));
+
+    return acosf(x);
 }
 
 __device__ float radians_to_degrees(float radian_value)
@@ -140,11 +138,13 @@ __device__ void update_bin(int *bin, float angle, int incrementor)
     atomicAdd(&bin[index], incrementor);
 }
 
-__global__ void collect_histograms(GalaxySet real, GalaxySet random, int *DD_histogram, int *DR_histogram, int *RR_histogram, int n)
+__global__ void collect_histograms(GalaxySet real, GalaxySet random, int *DD_histogram_collected, int *DR_histogram_collected, int *RR_histogram_collected, int n)
 {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
 
+    // Shared arrays are used to reduce duration of atomic adding when updating results.
+    // Each block uses its own set of arrays of histograms, equal to the size of the actual histogram arrays.
     __shared__ int shared_DD_histogram[BINS_TOTAL];
     __shared__ int shared_DR_histogram[BINS_TOTAL];
     __shared__ int shared_RR_histogram[BINS_TOTAL];
@@ -185,11 +185,12 @@ __global__ void collect_histograms(GalaxySet real, GalaxySet random, int *DD_his
         }
     __syncthreads();
 
+    // The section corresponding to the current block is updated in the collected histogram arrays.
     for (int i = threadIdx.x; i < BINS_TOTAL; i += blockDim.x)
     {
-        DD_histogram[blockIdx.x * BINS_TOTAL + i] = shared_DD_histogram[i];
-        DR_histogram[blockIdx.x * BINS_TOTAL + i] = shared_DR_histogram[i];
-        RR_histogram[blockIdx.x * BINS_TOTAL + i] = shared_RR_histogram[i];
+        DR_histogram_collected[blockIdx.x * BINS_TOTAL + i] = shared_DD_histogram[i];
+        DR_histogram_collected[blockIdx.x * BINS_TOTAL + i] = shared_DR_histogram[i];
+        RR_histogram_collected[blockIdx.x * BINS_TOTAL + i] = shared_RR_histogram[i];
     }
 }
 
@@ -204,6 +205,16 @@ __global__ void measure_galaxy_distribution(int *DD_histogram, int *DR_histogram
             continue;
 
         distribution[i] = (DD_histogram[i] - 2.0f * DR_histogram[i] + RR_histogram[i]) / RR_histogram[i];
+    }
+}
+
+void accumulate_histograms(int *DD_histogram_collected, int *DR_histogram_collected, int *RR_histogram_collected, int *DD_histogram, int *DR_histogram, int *RR_histogram, int n)
+{
+    for (int i = 0; i < n; i += 1)
+    {
+        DD_histogram[i % BINS_TOTAL] += DD_histogram_collected[i];
+        DR_histogram[i % BINS_TOTAL] += DR_histogram_collected[i];
+        RR_histogram[i % BINS_TOTAL] += RR_histogram_collected[i];
     }
 }
 
